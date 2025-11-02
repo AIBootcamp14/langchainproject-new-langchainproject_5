1f73b6d0f85b1c8d35cc9383d8a73a0ec592cf81
ieyeppo <ieyeppo.job@gmail.com>
2025-11-03 06:43:12 +0900

    chore: 실행 폴더 추가

---
+++ experiments/20251103/20251103_061644_session_019/database/queries.sql
@@ -0,0 +1,13 @@
+-- 실행 시간: 2025-11-03 06:16:46
+                            -- 도구: 
+                            -- 설명: 
+                        -- 파라미터: ('%BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding%',)
+-- 결과 개수: 1
+
+
+        SELECT paper_id, title, authors, abstract, publish_date
+        FROM papers
+        WHERE title ILIKE %s
+        LIMIT 1
+        ;
+
