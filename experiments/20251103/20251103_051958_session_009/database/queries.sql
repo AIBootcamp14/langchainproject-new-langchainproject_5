adf272f239da22eb51936456c13cce38336bab3b
ieyeppo <ieyeppo.job@gmail.com>
2025-11-03 09:34:13 +0900

    fix: 삭제된 experiments 폴더 복구

--- experiments/20251103/20251103_051958_session_009/database/queries.sql
+++
@@ -1,6 +0,0 @@
--- 실행 시간: 2025-11-03 05:23:27
-                            -- 도구: rag_glossary
-                            -- 설명: 용어집 검색
-                        
-SELECT term, definition, easy_explanation, hard_explanation, category FROM glossary WHERE term ILIKE %s;
-
