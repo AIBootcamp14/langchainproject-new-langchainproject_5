0edbfa5aa38345d3838571bbd013ad3f5fd6b01c
ieyeppo <ieyeppo.job@gmail.com>
2025-11-04 12:48:22 +0900

    refactor: Evaluation 모듈에서 create_evaluation_table export 제거
    - __init__.py의 import 및 __all__에서 create_evaluation_table 제거
    - 함수가 삭제되었으므로 export도 제거

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -7,7 +7,6 @@ LLM-as-a-Judge 방식을 사용한 답변 품질 평가 시스템
 
 from .evaluator import AnswerEvaluator
 from .storage import (
-    create_evaluation_table,
     save_evaluation_results,
     get_evaluation_results,
     get_evaluation_statistics
@@ -15,7 +14,6 @@ from .storage import (
 
 __all__ = [
     'AnswerEvaluator',
-    'create_evaluation_table',
     'save_evaluation_results',
     'get_evaluation_results',
     'get_evaluation_statistics'
